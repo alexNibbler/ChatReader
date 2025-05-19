b860de7c0e16b78c8d373711a5c22bd831209fb2
alex.hlyavich <alexhlyavich@gmail.com>
2025-05-19 20:34:28 +0300

    Initialize genai explicitly on start of the application

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import logging
 from flask import Flask
 from app.config import load_configurations, configure_logging
 from app.views import webhook_blueprint
+import app.services.genai_service as genai_service
 
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app = Flask(__name__)
 # Load configurations and logging settings
 load_configurations(app)
 configure_logging()
+genai_service.configure_gen_ai_service()
 
 # Import and register blueprints, if any
 app.register_blueprint(webhook_blueprint)
